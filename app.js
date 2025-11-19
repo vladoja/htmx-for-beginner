2ca8f9ec46e79895f0d6bd76638d5724d835c168
vladoja <dr.jalo2@gmail.com>
2025-11-19 23:27:33 +0100

    L5 Setting max id as global variable on init.

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ import createHomepageTemplate from './views/index.js';
 import createListTemplate from './views/list.js';
 import BOOKS_DATA from './data/data.js';
 
+
 // create app
 const app = express();
+var maxId = initMaxId(BOOKS_DATA);
+
 app.use(express.urlencoded({ extended: false }));
 
 // static assets
@@ -24,6 +27,9 @@ app.get('/books', (req, res) => {
 app.post('/books', (req, res) => {
   const { title, author } = req.body;
   const newBook = { title, author };
+  maxId++;
+  newBook.id = String(maxId);
+  console.log('Assigned ID to new book:', newBook.id);
   BOOKS_DATA.push(newBook);
   res.send(createListTemplate());
 });
@@ -35,3 +41,14 @@ app.post('/books', (req, res) => {
 app.listen(3000, () => {
   console.log('App listening on port 3000');
 });
+
+
+
+function initMaxId(data) {
+  let max = 0;
+  for (const { id } of data) {
+    const num = Number(id);
+    if (num > max) max = num;
+  }
+  return max;
+}
