6c79b749313f126fd9571856336442f078e5014d
vladoja <dr.jalo2@gmail.com>
2025-11-23 12:50:29 +0100

    L8 Alternative way via "closest li".

--- views/book.js
+++ views/book.js
@@ -3,7 +3,8 @@ const createBookTemplate = (book) => /*html*/ `
     <div class="details">
         <h3>${book.title}</h3>
         <p>${book.author}</p>
-        <button hx-delete="/books/${book.id}" hx-target="li[data-id='${book.id}']" hx-swap="outerHTML">Delete</button>
+        <!-- <button hx-delete="/books/${book.id}" hx-target="li[data-id='${book.id}']" hx-swap="outerHTML">Delete</button> -->
+        <button hx-delete="/books/${book.id}" hx-target="closest li" hx-swap="outerHTML">Delete</button>
     </div>
 </li>
 `;
